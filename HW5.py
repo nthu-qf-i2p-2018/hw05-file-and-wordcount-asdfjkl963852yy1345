ec6928692d969199feab2ea1067b0d69b6cce241
105023016 <37070067+asdfjkl963852yy1345@users.noreply.github.com>
2018-04-19 03:21:04 +0800

    HW555555555

---
+++ HW5.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 18 18:31:05 2018
+
+@author: acer
+"""
+
+import csv
+import json
+from collections import Counter
+import string
+
+def main(filename):
+    txtfile=open(filename)
+    lines=txtfile.readlines()
+    
+    all_words=[]
+    
+    for line in lines:
+        words=line.split()
+        
+        for word in words:
+            word=word.strip(string.punctuation)
+            
+            if word!=None and word!="":
+                all_words.append(word)
+                
+    count=Counter(all_words)
+    d=dict()
+    for a,b in count.most_common():
+        add_dict={a:b}
+        d.update(add_dict)
+        
+    with open("wordcount.csv" , "w") as csv_file:
+        for word,count in count.most_common():
+            print(word,count,file=csv_file,sep=",")
+    
+    
+    f=open("wordcount.json", "w") 
+    json.dump(d , f)
+    
+        
+            
+        
+    
+    
+
+                      
+    
+                    
+    
+            
